5362f3a55d8cfd3f47bf1f6ea6d43883c9742ca1
Prapat Jindarat <93991753+DoublePJ@users.noreply.github.com>
2024-05-01 10:11:48 +0700

    Add files via upload

---
+++ pages/index.php
@@ -0,0 +1,47 @@
+<headder>
+    <div class="box-menu">
+        <div class="logo">
+            <a href="index">
+                <h1><?= $header['title_menu']; ?></h1>
+            </a>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="index">หน้าแรก</a></li>
+                <li><a href="product">สินค้าทั้งหมด</a></li>
+                <li><a href="blog">บทความ</a></li>
+                <li><a href="about">เกี่ยวกับเรา</a></li>
+                <li><a href="contact">ติดต่อเรา</a></li>
+                <li><a href="login">เข้าสู่ระบบ</a></li>
+            </ul>
+        </div>
+    </div>
+
+    </header>
+
+    <section class="slide">
+        <!-- Swiper -->
+        <div class="swiper mySwiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="img" style="background-image: url(https://www.425degree.com/media/slideshow/cache/1500x645/catalog/category/425_exclusive/disney_tinkerbell/240403_distb_banner-all_01.jpg);"></div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="img" style="background-image: url(https://www.425degree.com/media/slideshow/cache/1500x645/campaign/promotion/240425_APR_425Day/BD_WebBanner_1920X826_1.jpg);"></div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="img" style="background-image: url(https://www.425degree.com/media/slideshow/cache/1500x645/catalog/category/case_galaxy-s24_series/240117_banner_s24-series_01.jpg);"></div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="img" style="background-image: url(https://www.425degree.com/media/slideshow/cache/1500x645/catalog/category/case_iphone15_series/230913_banner_case_ip15_01.jpg);"></div>
+                </div>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-pagination"></div>
+        </div>
+    </section>
+
+
+
+</headder>
